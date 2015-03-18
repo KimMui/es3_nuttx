2bda8b8d4b5e1e5bfb24247b8fe7b6ef60130cf2
Fabien Parent <parent.f@gmail.com>
2015-03-18 11:30:22 +0100

    greybus: usb: add usb-phy

---
+++ nuttx/drivers/greybus/usb-gb.h
@@ -0,0 +1,112 @@
+#ifndef __USB_GB_H__
+#define __USB_GB_H__
+
+#include <nuttx/greybus/types.h>
+
+#define GB_USB_HOST_DIR_OUT                 0
+#define GB_USB_HOST_DIR_IN                  0x80
+
+#define GB_USB_HOST_PIPE_ISOCHRONOUS        0
+#define GB_USB_HOST_PIPE_INTERRUPT          1
+#define GB_USB_HOST_PIPE_CONTROL            2
+#define GB_USB_HOST_PIPE_BULK               3
+
+#define gb_usb_host_pipein(pipe)            ((pipe) & GB_USB_HOST_DIR_IN)
+#define gb_usb_host_pipeout(pipe)           (!gb_usb_host_pipein(pipe))
+
+#define gb_usb_host_pipedevice(pipe)        (((pipe) >> 8) & 0x7f)
+#define gb_usb_host_pipeendpoint(pipe)      (((pipe) >> 15) & 0xf)
+
+#define gb_usb_host_pipetype(pipe)          (((pipe) >> 30) & 3)
+#define gb_usb_host_pipeisoc(pipe) \
+    (gb_usb_host_pipetype((pipe)) == GB_USB_HOST_PIPE_ISOCHRONOUS)
+#define gb_usb_host_pipeint(pipe) \
+    (gb_usb_host_pipetype((pipe)) == GB_USB_HOST_PIPE_INTERRUPT)
+#define gb_usb_host_pipecontrol(pipe) \
+    (gb_usb_host_pipetype((pipe)) == GB_USB_HOST_PIPE_CONTROL)
+#define gb_usb_host_pipebulk(pipe) \
+    (gb_usb_host_pipetype((pipe)) == GB_USB_HOST_PIPE_BULK)
+
+#define GB_USB_HOST_URB_SHORT_NOT_OK        0x0001
+#define GB_USB_HOST_URB_ISO_ASAP            0x0002
+#define GB_USB_HOST_URB_NO_TRANSFER_DMA_MAP 0x0004
+#define GB_USB_HOST_URB_NO_FSBR             0x0020
+#define GB_USB_HOST_URB_ZERO_PACKET         0x0040
+#define GB_USB_HOST_URB_NO_INTERRUPT        0x0080
+#define GB_USB_HOST_URB_FREE_BUFFER         0x0100
+
+#define GB_USB_HOST_URB_DIR_IN              0x0200
+#define GB_USB_HOST_URB_DIR_OUT             0
+#define GB_USB_HOST_URB_DIR_MASK            GB_USB_HOST_URB_DIR_IN
+
+#define GB_USB_HOST_ENDPOINT_XFERTYPE_MASK  0x03
+#define GB_USB_HOST_ENDPOINT_XFER_CONTROL   0
+#define GB_USB_HOST_ENDPOINT_XFER_ISOC      1
+#define GB_USB_HOST_ENDPOINT_XFER_BULK      2
+#define GB_USB_HOST_ENDPOINT_XFER_INT       3
+#define GB_USB_HOST_ENDPOINT_MAX_ADJUSTABLE 0x80
+
+/* Version of the Greybus USB protocol we support */
+#define GB_USB_VERSION_MAJOR		0x00
+#define GB_USB_VERSION_MINOR		0x01
+
+/* Greybus USB request types */
+#define GB_USB_TYPE_INVALID             0x00
+#define GB_USB_TYPE_PROTOCOL_VERSION    0x01
+#define GB_USB_TYPE_HCD_STOP            0x02
+#define GB_USB_TYPE_HCD_START           0x03
+#define GB_USB_TYPE_URB_ENQUEUE         0x04
+#define GB_USB_TYPE_URB_DEQUEUE         0x05
+#define GB_USB_TYPE_ENDPOINT_DISABLE    0x06
+#define GB_USB_TYPE_HUB_CONTROL         0x07
+#define GB_USB_TYPE_GET_FRAME_NUMBER    0x08
+#define GB_USB_TYPE_HUB_STATUS_DATA     0x09
+
+struct gb_usb_proto_version_response {
+	__u8	major;
+	__u8	minor;
+};
+
+struct gb_usb_hub_control_request {
+	__le16 typeReq;
+	__le16 wValue;
+	__le16 wIndex;
+	__le16 wLength;
+};
+
+struct gb_usb_hub_control_response {
+	__le32 status;
+	__u8 buf[0];
+};
+
+struct gb_usb_urb_enqueue_request {
+	__le32 pipe;
+	__le32 transfer_flags;
+	__le32 transfer_buffer_length;
+	__le32 maxpacket;
+	__le32 interval;
+	__le64 hcpriv_ep;
+	__le32 number_of_packets;
+	__u8   dev_speed;
+	__le32 devnum;
+	__le32 dev_ttport;
+	u8 setup_packet[8];
+	u8 payload[0];
+};
+
+struct gb_usb_urb_enqueue_response {
+	__le32 status;
+	__le32 actual_length;
+	u8 payload[0];
+};
+
+struct gb_usb_urb_dequeue_request {
+	__le64 hcpriv_ep;
+};
+
+struct gb_usb_endpoint_disable_request {
+	__le64 hcpriv_ep;
+};
+
+#endif /* __USB_GB_H__ */
+
---
+++ nuttx/drivers/greybus/usb.c
@@ -0,0 +1,251 @@
+/*
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <nuttx/greybus/greybus.h>
+#include <nuttx/usb.h>
+#include "usb-gb.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static uint8_t gb_usb_protocol_version(struct gb_operation *operation)
+{
+    struct gb_usb_proto_version_response *response;
+
+    response = gb_operation_alloc_response(operation, sizeof(*response));
+    if (!response)
+        return GB_OP_NO_MEMORY;
+
+    response->major = GB_USB_VERSION_MAJOR;
+    response->minor = GB_USB_VERSION_MINOR;
+    return GB_OP_SUCCESS;
+}
+
+static uint8_t gb_usb_hcd_stop(struct gb_operation *operation)
+{
+    printf("%s()\n", __func__);
+    hcd_stop();
+    return GB_OP_SUCCESS;
+}
+
+static uint8_t gb_usb_hcd_start(struct gb_operation *operation)
+{
+    printf("%s()\n", __func__);
+    hcd_start();
+    return GB_OP_SUCCESS;
+}
+
+static uint8_t gb_usb_hub_control(struct gb_operation *operation)
+{
+    struct gb_usb_hub_control_response *response;
+    struct gb_usb_hub_control_request *request =
+        gb_operation_get_request_payload(operation);
+
+    response =
+        gb_operation_alloc_response(operation,
+                                    sizeof(*response) + request->wLength);
+    if (!response)
+        return GB_OP_NO_MEMORY;
+
+    response->status = hub_control(request->typeReq, request->wValue,
+                                   request->wIndex, (char*) response->buf,
+                                   request->wLength);
+
+    printf("%s(%hX, %hX, %hX, %hX)", __func__, request->typeReq,
+           request->wValue, request->wIndex, request->wLength);
+
+    return GB_OP_SUCCESS;
+}
+
+static void gb_usb_urb_complete(struct urb *urb)
+{
+    sem_post(&urb->semaphore);
+}
+
+static uint8_t gb_usb_urb_enqueue(struct gb_operation *operation)
+{
+    printf("%s()\n", __func__);
+
+    int retval;
+    int op_status = GB_OP_SUCCESS;
+    struct urb *urb;
+    struct gb_usb_urb_enqueue_response *response;
+    struct gb_usb_urb_enqueue_request *request =
+        gb_operation_get_request_payload(operation);
+
+    urb = malloc(sizeof(*urb)); // FIXME: ref
+    if (!urb)
+        return GB_OP_NO_MEMORY;
+
+    memset(urb, 0, sizeof(*urb));
+
+    sem_init(&urb->semaphore, 0, 0);
+    urb->urb = NULL;
+    urb->pipe = request->pipe;
+    urb->length = request->transfer_buffer_length;
+    urb->maxpacket = request->maxpacket;
+    //urb->hcpriv_ep = request->hcpriv_ep;
+    urb->interval = request->interval;
+    urb->dev_speed = request->dev_speed;
+    urb->dev_ttport = request->dev_ttport;
+    urb->devnum = request->devnum;
+    urb->complete = gb_usb_urb_complete;
+    memcpy(urb->setup_packet, request->setup_packet, sizeof(urb->setup_packet));
+
+    printf("urb->devnum = %d\n", urb->devnum);
+    printf("urb->dev_ttport = %d\n", urb->dev_ttport);
+    printf("urb->dev_speed = %hhu\n", urb->dev_speed);
+
+    if (request->transfer_buffer_length) {
+        urb->buffer = malloc(request->transfer_buffer_length);
+        if (!urb->buffer) {
+            op_status = GB_OP_NO_MEMORY;
+            goto end;
+        }
+
+        memcpy(urb->buffer, request->payload, request->transfer_buffer_length);
+    }
+
+    if (!(request->transfer_flags & GB_USB_HOST_URB_NO_INTERRUPT))
+        urb->flags |= USB_URB_GIVEBACK_ASAP;
+
+    if (request->transfer_flags & GB_USB_HOST_URB_ZERO_PACKET)
+        urb->flags |= USB_URB_SEND_ZERO_PACKET;
+
+    retval = urb_enqueue(urb);
+    if (retval) {
+        printf("%s() : %d = %d\n", __func__, __LINE__, retval);
+        op_status = GB_OP_UNKNOWN_ERROR;
+        goto end;
+    }
+
+    sem_wait(&urb->semaphore);
+
+    response =
+        gb_operation_alloc_response(operation,
+                                    sizeof(*response) + urb->actual_length);
+    if (!response) {
+        op_status = GB_OP_NO_MEMORY;
+        goto end;
+    }
+
+    memset(response, 0, sizeof(*response) + urb->actual_length);
+
+    printf("\nGot a response back from device: %d %d\n",
+           urb->actual_length,
+           urb->status);
+
+    response->actual_length = urb->actual_length;
+    response->status = urb->status;
+    memcpy(response->payload, urb->buffer, urb->actual_length);
+
+#if TX_DEBUG
+    {
+        int i;
+        if (urb->actual_length) {
+            printf("buffer:\n");
+            for (i = 0; i < urb->actual_length; i++) {
+                printf("%hhx ", ((char*) urb->buffer)[i]);
+            }
+            printf("\n");
+        }
+    }
+#endif
+
+end:
+    free(urb->buffer);
+    free(urb);
+
+    return op_status;
+}
+
+#if 0
+static uint8_t gb_usb_urb_dequeue(struct gb_operation *operation)
+{
+    struct rpc_urb_dequeue *req = gb_operation_get_request_payload(operation);
+
+    printf("%s()\n", __func__);
+
+    urb_dequeue(req->urb);
+
+    return GB_OP_SUCCESS;
+}
+#endif
+
+static uint8_t gb_usb_endpoint_disable(struct gb_operation *operation)
+{
+    void *ep;
+    struct gb_usb_endpoint_disable_request *request =
+        gb_operation_get_request_payload(operation);
+
+    printf("%s()\n", __func__);
+
+#if 1
+    ep = (void*) ((uint32_t) request->hcpriv_ep); // FIXME downcasting
+    printf("ep = %X\n", ep);
+    if (!endpoint_disable(ep))
+        return GB_OP_UNKNOWN_ERROR;
+#endif
+    return GB_OP_SUCCESS;
+}
+
+static int gb_usb_init(unsigned int cport)
+{
+    return hcd_init();
+}
+
+static void gb_usb_exit(unsigned int cport)
+{
+    hcd_exit();
+}
+
+static struct gb_operation_handler gb_usb_handlers[] = {
+    GB_HANDLER(GB_USB_TYPE_PROTOCOL_VERSION, gb_usb_protocol_version),
+    GB_HANDLER(GB_USB_TYPE_HCD_STOP, gb_usb_hcd_stop),
+    GB_HANDLER(GB_USB_TYPE_HCD_START, gb_usb_hcd_start),
+    GB_HANDLER(GB_USB_TYPE_URB_ENQUEUE, gb_usb_urb_enqueue),
+//    GB_HANDLER(GB_USB_TYPE_URB_DEQUEUE, gb_usb_urb_dequeue),
+    GB_HANDLER(GB_USB_TYPE_ENDPOINT_DISABLE, gb_usb_endpoint_disable),
+    GB_HANDLER(GB_USB_TYPE_HUB_CONTROL, gb_usb_hub_control),
+//    GB_HANDLER(GB_USB_TYPE_GET_FRAME_NUMBER, gb_usb_get_frame_number),
+//    GB_HANDLER(GB_USB_TYPE_HUB_STATUS_DATA, gb_usb_hub_status_data),
+};
+
+struct gb_driver usb_driver = {
+    .op_handlers = (struct gb_operation_handler*) gb_usb_handlers,
+    .op_handlers_count = ARRAY_SIZE(gb_usb_handlers),
+
+    .init = gb_usb_init,
+    .exit = gb_usb_exit,
+};
+
+void gb_usb_register(int cport)
+{
+    gb_register_driver(cport, &usb_driver);
+}
