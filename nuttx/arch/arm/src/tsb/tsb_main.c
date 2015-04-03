a966e8260280392641ad9438ceb855a6f40c07de
Fabien Parent <parent.f@gmail.com>
2015-04-03 14:39:58 -0400

    wip: debug

--- nuttx/arch/arm/src/tsb/tsb_main.c
+++ nuttx/arch/arm/src/tsb/tsb_main.c
@@ -12,17 +12,32 @@
 #include "up_arch.h"
 #include "up_internal.h"
 
+#include <stdio.h>
+#include "usb/dwc_os.h"
+
+void test_callback(void *data)
+{
+    printf("callback %s\n", __func__);
+}
+
 int main(void)
 {
     printf("\n\n===== USB Host Dev FW =====\n");
 
     tsb_gpio_register();
 
-    int gb_uart_init(void);
-    gb_uart_init();
+//    int gb_uart_init(void);
+//    gb_uart_init();
 
-    void gb_usb_register(int cport);
-    gb_usb_register(0);
+//    void gb_usb_register(int cport);
+//    gb_usb_register(0);
+
+    {
+        dwc_timer_t *timer = DWC_TIMER_ALLOC("test", test_callback, NULL);
+        DWC_TIMER_SCHEDULE(timer, 10);
+        //DWC_TIMER_CANCEL
+        //DWC_TIMER_FREE(timer);
+    }
 
     return 0;
 }
