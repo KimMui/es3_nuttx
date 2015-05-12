a9f5ed466170ae600b85b32b19980bac78a766c3
Fabien Parent <fparent@baylibre.com>
2015-05-12 12:19:26 +0200

    usb: add urb enqueue support

--- nuttx/include/nuttx/usb.h
+++ nuttx/include/nuttx/usb.h
@@ -33,17 +33,122 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <semaphore.h>
 #include <errno.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include <nuttx/device.h>
+#include <arch/atomic.h>
 
 #define DEVICE_TYPE_USB_HCD     "usb-hcd"
 #define DEVICE_TYPE_HSIC_DEVICE "hsic-device"
 
+#define USB_URB_GIVEBACK_ASAP           1
+#define USB_URB_SEND_ZERO_PACKET        2
+
+#define USB_HOST_DIR_OUT                0
+#define USB_HOST_DIR_IN                 0x80
+
+#define USB_HOST_PIPE_ISOCHRONOUS       0
+#define USB_HOST_PIPE_INTERRUPT         1
+#define USB_HOST_PIPE_CONTROL           2
+#define USB_HOST_PIPE_BULK              3
+
+#define usb_host_pipein(pipe)           ((pipe) & USB_HOST_DIR_IN)
+#define usb_host_pipeout(pipe)          (!usb_host_pipein(pipe))
+
+#define usb_host_pipedevice(pipe)       (((pipe) >> 8) & 0x7f)
+#define usb_host_pipeendpoint(pipe)     (((pipe) >> 15) & 0xf)
+
+#define usb_host_pipetype(pipe)         (((pipe) >> 30) & 3)
+#define usb_host_pipeisoc(pipe) \
+    (usb_host_pipetype((pipe)) == USB_HOST_PIPE_ISOCHRONOUS)
+#define usb_host_pipeint(pipe) \
+    (usb_host_pipetype((pipe)) == USB_HOST_PIPE_INTERRUPT)
+#define usb_host_pipecontrol(pipe) \
+    (usb_host_pipetype((pipe)) == USB_HOST_PIPE_CONTROL)
+#define usb_host_pipebulk(pipe) \
+    (usb_host_pipetype((pipe)) == USB_HOST_PIPE_BULK)
+
+#define USB_HOST_ENDPOINT_XFER_CONTROL  0
+#define USB_HOST_ENDPOINT_XFER_ISOC     1
+#define USB_HOST_ENDPOINT_XFER_BULK     2
+#define USB_HOST_ENDPOINT_XFER_INT      3
+
+struct urb;
+typedef void (*urb_complete_t)(struct urb *urb);
+
+struct urb {
+    atomic_t refcount;
+    sem_t semaphore;
+    urb_complete_t complete;
+
+    void *urb;
+    unsigned int pipe;
+    unsigned int flags;
+
+    int status;
+    int dev_speed;
+    int devnum;
+    int dev_ttport;
+
+    size_t length;
+    size_t actual_length;
+
+    unsigned int maxpacket;
+
+    int interval;
+
+    uint8_t setup_packet[8];
+    void *buffer;
+
+    void *hcpriv;
+    void *hcpriv_ep;
+};
+
+static inline struct urb *urb_create(void) // FIXME
+{
+    struct urb *urb;
+
+    urb = zalloc(sizeof(*urb));
+    if (!urb) {
+        return NULL;
+    }
+
+    atomic_init(&urb->refcount, 1);
+    sem_init(&urb->semaphore, 0, 0);
+    return urb;
+}
+
+static inline void urb_ref(struct urb *urb)
+{
+    DEBUGASSERT(urb);
+    atomic_inc(&urb->refcount);
+}
+
+static inline void urb_unref(struct urb *urb)
+{
+    if (!urb) {
+        return;
+    }
+
+    if (atomic_dec(&urb->refcount)) {
+        return;
+    }
+
+    free(urb);
+}
+
+static inline void urb_destroy(struct urb *urb)
+{
+    urb_unref(urb);
+}
+
 struct device_usb_hcd_type_ops {
     int (*start)(struct device *dev);
     void (*stop)(struct device *dev);
+    int (*urb_enqueue)(struct device *dev, struct urb *urb);
     int (*hub_control)(struct device *dev, uint16_t typeReq, uint16_t wValue,
                        uint16_t wIndex, char *buf, uint16_t wLength);
 };
@@ -115,6 +220,31 @@ static inline int device_usb_hcd_hub_control(struct device *dev,
         return dev->driver->ops->type_ops.usb_hcd->hub_control(dev, typeReq,
                                                                wValue, wIndex,
                                                                buf, wLength);
+    }
+
+    return -ENOSYS;
+}
+
+/**
+ * Send urbs
+ *
+ * @param dev HCD device
+ * @param urb URB to send
+ * @return 0 if successful
+ */
+static inline int device_usb_hcd_urb_enqueue(struct device *dev,
+                                             struct urb *urb)
+{
+    DEBUGASSERT(dev);
+    DEBUGASSERT(dev->driver && dev->driver->ops &&
+                dev->driver->ops->type_ops.usb_hcd);
+
+    if (dev->state != DEVICE_STATE_OPEN) {
+        return -ENODEV;
+    }
+
+    if (dev->driver->ops->type_ops.usb_hcd->urb_enqueue) {
+        return dev->driver->ops->type_ops.usb_hcd->urb_enqueue(dev, urb);
     }
 
     return -ENOSYS;
